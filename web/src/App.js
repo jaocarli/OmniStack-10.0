eadb8a6ae344ab5beaf8daaffec7d2e618d13230
João Carli <46573038+fl4m3x@users.noreply.github.com>
2020-01-15 21:28:06 -0300

    João Carli

--- web/src/App.js
+++ web/src/App.js
@@ -1,51 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+
 import api from './services/api';
 
 import './global.css';
 import './App.css';
-import './Sidebar.css';
-import './Main.css';
+import './Sidebar.css'
+import './Main.css'
 
 import DevForm from './components/DevForm';
 import DevItem from './components/DevItem';
+
 
 function App() {
   const [devs, setDevs] = useState([]);
 
   useEffect(() => {
     async function loadDevs() {
-      const response = await api.get('/devs')
+      const response = await api.get('/devs');
 
       setDevs(response.data);
     }
 
     loadDevs();
-  }, []);
-  
+  }, [devs]);
+
   async function handleAddDev(data) {
-
-const response = await api.post('/devs', data)
-
-setDevs([...devs, response.data]);
-
+    const response = await api.post('/devs', data);
+    const hasDev = devs.filter(dev => dev.github_name === data.github_name);
+    
+    if(!hasDev) {
+      setDevs([...devs, response.data])
+    }  
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <DevForm onSubit={handleAddDev} />
+        <DevForm onSubmit={handleAddDev} />
       </aside>
-    
-    <main>
-      <ul>
-        {devs.map(dev => (
-          <DevItem key={dev._id} dev={dev} />
-        ))}
-        </ul> 
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}          
+        </ul>
       </main>
     </div>
-    
   );
 }
 
